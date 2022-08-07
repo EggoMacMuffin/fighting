d90c1c97718babb417d3c70e190ba92a93b330fb
AnimeGirl110 <djoctowoomy@gmail.com>
2022-08-07 09:39:51 -0400

    added OnTriggerEnter blocks and renamed some variables.

--- Assets/Leah's Character/Script/GuardBehavior.cs
+++ Assets/Leah's Character/Script/GuardBehavior.cs
@@ -21,9 +21,11 @@ public class GuardBehavior : MonoBehaviour
     private float kickDist = 1.3f;
     private float punchDist = 1.7f;
     private float headbuttDist = 1.8f;
-    public bool hasAddedMoves = false;
+    public bool hasAddedMove = false;
 
-    private List<string> moves = new List<string>(5);
+    private List<string> attacks = new List<string>(5);
+    private List<string> blocks = new List<string>(5);
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -36,43 +38,61 @@ public class GuardBehavior : MonoBehaviour
         playerRightHand = GameObject.FindWithTag("Player_Right_Hand").transform;
         playerRightLeg = GameObject.FindWithTag("Player_Right_Leg").transform;
 
-        moves.Add("Headbutt");
-        moves.Add("PunchElbow");
-        moves.Add("PunchHook");
-        moves.Add("PunchRight");
-        moves.Add("Kick");
+        attacks.Add("Headbutt");
+        attacks.Add("PunchElbow");
+        attacks.Add("PunchHook");
+        attacks.Add("PunchRight");
+        attacks.Add("Kick");
+
+        blocks.Add("Duck");
+        blocks.Add("BlockRight");
+        blocks.Add("BlockLeft");
     }
 
     void Update()
     {
-        bool isAttacking = false;
-        for (int i = 0; i < moves.Count; i++) {
-            if (animator.GetCurrentAnimatorStateInfo(0).IsName(moves[i])) {
-                //isAttacking is false for multiple frames, letting the code add multiple attacks to the queue
-                isAttacking = true;
-                hasAddedMoves = false;
-                //hasAddedMoves basically ensures a trigger is set ONCE after each attack animation
+        bool isMakingMove = false;
+        for (int i = 0; i < attacks.Count; i++) {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName(attacks[i])) {
+                //isMakingMove is false for multiple frames, letting the code add multiple attacks to the queue
+                isMakingMove = true;
+                hasAddedMove = false;
+                //hasAddedMove basically ensures a trigger is set ONCE after each attack animation
+            }
+        } 
+        for (int i = 0; i < blocks.Count; i++) {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName(blocks[i])) {
+                //isMakingMove is false for multiple frames, letting the code add multiple attacks to the queue
+                isMakingMove = true;
+                hasAddedMove = false;
+                //hasAddedMove basically ensures a trigger is set ONCE after each attack animation
             }
         }
-        if (isAttacking == false) {
-            //the isAttacking thing is to make sure the AI "updates" faster-- otherwise it will continue to attack even if
+        for (int i = 0; i < blocks.Count; i++) {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName(blocks[i])) {
+                isMakingMove = true;
+                hasAddedMove = false;
+            }
+        }
+        if (isMakingMove == false) {
+            //the isMakingMove thing is to make sure the AI "updates" faster-- otherwise it will continue to attack even if
             // the player is away from its target zone (or not use attacks that have a smaller proximity range)
             List<string> possibleMoves = new List<string>();
 
             if (Vector3.Distance(transform.position, playerTorso.position) <= headbuttDist) {
-                possibleMoves.Add(moves[0]);
-                possibleMoves.Add(moves[1]);    
+                possibleMoves.Add(attacks[0]);
+                possibleMoves.Add(attacks[1]);    
                 if (Vector3.Distance(transform.position, playerTorso.position) <= punchDist) {
-                    possibleMoves.Add(moves[2]);
-                    possibleMoves.Add(moves[3]);  
+                    possibleMoves.Add(attacks[2]);
+                    possibleMoves.Add(attacks[3]);  
                     if (Vector3.Distance(transform.position, playerTorso.position) <= kickDist) {
-                        possibleMoves.Add(moves[4]);
+                        possibleMoves.Add(attacks[4]);
                     }
                 }
             }
-            if (possibleMoves.Count > 0 && !hasAddedMoves) {
+            if (possibleMoves.Count > 0 && !hasAddedMove) {
                 animator.SetTrigger(possibleMoves[Random.Range(0, possibleMoves.Count)]);
-                hasAddedMoves = true;
+                hasAddedMove = true;
             }
         }
         //possibility to add enemy movement down here
@@ -80,8 +100,12 @@ public class GuardBehavior : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Guard_Player (Test)") {
-            //todo
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name == "Guard_Player") {
+            if (!hasAddedMove) {
+                animator.SetTrigger(blocks[Random.Range(0, blocks.Count)]);
+                hasAddedMove = true;
+            }
         }
     }
 }
